f2f211f839715c0943ac88ddf8424009fdd81252
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-10-25 08:20:43 -0300

    feat: add basic header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script>
   import "../app.css";
+	import Header from "./header.svelte";
 
   let { children } = $props();
 </script>
 
+<Header />
+
 {@render children()}
+
---
+++ src/routes/header.svelte
@@ -0,0 +1,21 @@
+<header>
+  <h1>TreeLineage</h1>
+  <button>
+    Add new Person +
+  </button>
+  <button>
+    Share
+  </button>
+</header>
+
+<style>
+  header {
+    position: fixed;
+    top: 0;
+    z-index: 10;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    padding: 0.5rem 1rem;
+  }
+</style>
