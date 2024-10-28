eb9dec5e237cc2b52d162092b7af2549a57459e8
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-10-28 15:10:51 -0300

    feat: centralize header

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -4,7 +4,7 @@
 	import CreateFamilyNodeButton from './create-family-node-button.svelte';
 </script>
 
-<header class="fixed top-0 z-40 flex w-full justify-between bg-background px-4 py-2">
+<header class="fixed top-0 z-40 flex w-full justify-between items-center bg-background px-4 py-2">
 	<h1>TreeLineage</h1>
 	<CreateFamilyNodeButton />
 	<div class="flex space-x-2">
