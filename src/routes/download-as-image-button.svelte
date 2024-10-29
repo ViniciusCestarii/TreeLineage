b5cea653072724ede7322f4d9a7c1eca6a299691
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-10-29 15:36:49 -0300

    feat: add loading feedback for saving as image

--- src/routes/download-as-image-button.svelte
+++ src/routes/download-as-image-button.svelte
@@ -5,18 +5,19 @@
 	import { mode } from 'mode-watcher';
 
 	const nodes = useNodes();
-
 	const imageWidth = 1024;
 	const imageHeight = 768;
+	let loading = $state(false);
 
-	function handleClick() {
+	async function handleClick() {
 		const nodesBounds = getNodesBounds($nodes);
 		const viewport = getViewportForBounds(nodesBounds, imageWidth, imageHeight, 0.5, 2.0, 0.2);
-
 		const viewportDomNode = document.querySelector<HTMLElement>('.svelte-flow__viewport')!;
 
 		if (viewport) {
-			toPng(viewportDomNode, {
+			loading = true;
+
+			const dataUrl = await toPng(viewportDomNode, {
 				backgroundColor: $mode === 'dark' ? '#111' : '#fff',
 				width: imageWidth,
 				height: imageHeight,
@@ -25,14 +26,20 @@
 					height: `${imageHeight}px`,
 					transform: `translate(${viewport.x}px, ${viewport.y}px) scale(${viewport.zoom})`
 				}
-			}).then((dataUrl) => {
-				const link = document.createElement('a');
-				link.download = 'svelte-flow.png';
-				link.href = dataUrl;
-				link.click();
 			});
+			const link = document.createElement('a');
+			link.download = 'svelte-flow.png';
+			link.href = dataUrl;
+			link.click();
+			loading = false;
 		}
 	}
 </script>
 
-<Button onclick={handleClick} variant="outline">Download</Button>
+<Button class="min-w-[6.5rem]" onclick={handleClick} variant="outline" disabled={loading}>
+	{#if loading}
+		Loading...
+	{:else}
+		Download
+	{/if}
+</Button>
