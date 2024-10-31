67ca62bc4aa03e0d7c4b3a7863eee4fa5e7d711a
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-10-31 11:55:37 -0300

    refact: format

--- src/routes/collab.svelte
+++ src/routes/collab.svelte
@@ -1,50 +1,47 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import { io } from 'socket.io-client';
+	import { onMount } from 'svelte';
+	import { io } from 'socket.io-client';
 	import ThemeSwitch from '$lib/components/theme-switch.svelte';
 
-  let dotPositions: Array<{ x: number; y: number }> = $state([]);
-  let lastSentTime = $state(0);
+	let dotPositions: Array<{ x: number; y: number }> = $state([]);
+	let lastSentTime = $state(0);
 
-  const sendInterval = 100
-  
-  onMount(() => {
-    const socket = io("http://localhost:3001");
+	const sendInterval = 100;
 
-    const handleMouseMove = (event: MouseEvent) => {
-      const mouseData = {
-        x: event.clientX,
-        y: event.clientY,
-      };
+	onMount(() => {
+		const socket = io('http://localhost:3001');
 
-      const currentTime = Date.now();
-      if (currentTime - lastSentTime > sendInterval) {
-        socket.emit("mouseMove", mouseData);
-        lastSentTime = currentTime;
-      }
-    };
+		const handleMouseMove = (event: MouseEvent) => {
+			const mouseData = {
+				x: event.clientX,
+				y: event.clientY
+			};
 
-    window.addEventListener("mousemove", handleMouseMove);
+			const currentTime = Date.now();
+			if (currentTime - lastSentTime > sendInterval) {
+				socket.emit('mouseMove', mouseData);
+				lastSentTime = currentTime;
+			}
+		};
 
-    socket.on("mouseMove", (positions) => {
-      dotPositions.push(positions);
-    });
+		window.addEventListener('mousemove', handleMouseMove);
 
-    return () => {
-      window.removeEventListener("mousemove", handleMouseMove);
-      socket.disconnect();
-    };
-  });
+		socket.on('mouseMove', (positions) => {
+			dotPositions.push(positions);
+		});
 
-  $inspect(dotPositions);
+		return () => {
+			window.removeEventListener('mousemove', handleMouseMove);
+			socket.disconnect();
+		};
+	});
+
+	$inspect(dotPositions);
 </script>
 
-
-  {#each dotPositions as pos (Math.random())} <!-- Unique key for each position -->
-    <div
-      class="absolute size-10 z-50"
-      style="left: {pos.x}px; top: {pos.y}px;"
-    >
-    <ThemeSwitch />
-  </div>
-  {/each}
+{#each dotPositions as pos (Math.random())}
+	<!-- Unique key for each position -->
+	<div class="absolute z-50 size-10" style="left: {pos.x}px; top: {pos.y}px;">
+		<ThemeSwitch />
+	</div>
+{/each}
--- src/server.ts
+++ src/server.ts
@@ -1,45 +1,42 @@
-import http from "http";
-import { Server } from "socket.io";
+import http from 'http';
+import { Server } from 'socket.io';
 
 const httpServer = http.createServer((req, res) => {
-  // Define the routes
-  if (req.method === "GET" && req.url === "/") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Server is running ...");
-  } else {
-    // Handle 404 Not Found
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("404 Not Found");
-  }
+	// Define the routes
+	if (req.method === 'GET' && req.url === '/') {
+		res.writeHead(200, { 'Content-Type': 'text/plain' });
+		res.end('Server is running ...');
+	} else {
+		// Handle 404 Not Found
+		res.writeHead(404, { 'Content-Type': 'text/plain' });
+		res.end('404 Not Found');
+	}
 });
 
-const corsOrigins = [
-  process.env.CLIENT_URL ?? "http://localhost:5173",
-  "http://localhost:5173",
-]
-console.log(`cors origins: `, corsOrigins)
+const corsOrigins = [process.env.CLIENT_URL ?? 'http://localhost:5173', 'http://localhost:5173'];
+console.log(`cors origins: `, corsOrigins);
 
 const io = new Server(httpServer, {
-  cors: {
-    origin: corsOrigins,
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
+	cors: {
+		origin: corsOrigins,
+		methods: ['GET', 'POST'],
+		credentials: true
+	}
 });
 
-io.on("connection", (socket) => {
-  console.log("A client connected");
+io.on('connection', (socket) => {
+	console.log('A client connected');
 
-  socket.on("mouseMove", (data) => {
-    console.log("mouseMove", data);
-    io.emit("mouseMove", data);
-  });
+	socket.on('mouseMove', (data) => {
+		console.log('mouseMove', data);
+		io.emit('mouseMove', data);
+	});
 
-  socket.on("disconnect", (reason) => {
-    console.log("A client disconnected. Reason:", reason);
-  });
+	socket.on('disconnect', (reason) => {
+		console.log('A client disconnected. Reason:', reason);
+	});
 });
 const PORT = process.env.PORT || 3001;
 httpServer.listen(PORT, async () => {
-  console.log(`socket.io server is running on port ${PORT}`);
+	console.log(`socket.io server is running on port ${PORT}`);
 });
